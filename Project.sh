bbd0d455a1a234829d736665858d8884a0e91f47
arcadesArena <shreyanshu.dps@gmail.com>
2022-02-09 00:11:51 +0530

    formatting and rearranging

--- Project.sh
+++ Project.sh
@@ -1,55 +1,76 @@
-#! /bin/bash
- sudo apt-get install figlet
- figlet 'Welcome'
-
-
-echo User Type :
-echo 1.student 
-echo 2.professor
-echo Enter your Profession : 
+#!/bin/bash
+echo "PREPARING SETUP"
+sudo apt-get install -y -qq figlet > /dev/nul
+sudo apt install -y -qq snapd > /dev/nul
+clear
+figlet 'Welcome' | ./color_it.sh 1
+echo "Setup account for type :"
+echo "1.student"
+echo "2.professor"
+echo "Enter type : "
 read Profession
+
+func_commonInstall() {
+    echo "INFO: installing libre office"
+    #sudo apt-get install -qq -y libreoffice
+    echo "INFO: installing google chrome"
+    # wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    # sudo apt install -y -qq ./google-chrome-stable_current_amd64.deb
+    # rm google-chrome-stable_current_amd64.deb
+    echo "INFO: installing vlc media player"
+    # sudo snap install -y -qq vlc    
+    echo "INFO: installing zoom"
+    # wget https://zoom.us/client/latest/zoom_amd64.deb
+    # sudo apt install -y -qq ./zoom_amd64.deb
+}
+func_student() {
+    echo "CONFIGURING SYSTEM FOR A STUDENT"
+    sudo -i
+    echo "INFO: creating user student"
+    # adduser student
+    exit
+    echo "INFO: installing visual studio code"
+    #sudo apt-get install -qq -y code --classic
+    echo "INFO: installing git"
+    # sudo apt-get install -y -qq git
+    echo "INFO: installing jdk"
+    # sudo apt-get install -y -qq openjdk-11-jdk
+    echo "INFO: installing whatsapp"
+    # sudo snap install -y -qq whatsapp-for-linux
+    echo "INFO: installing obs"
+    # sudo apt install -y -qq ffmpeg
+    # sudo apt install -y -qq v4l2loopback-dkms
+    # sudo add-apt-repository ppa:obsproject/obs-studio
+    # sudo apt update
+    # sudo apt install -y -qq obs-studio
+    echo "INFO: installing discord"
+    # sudo snap install -y -qq discord
+}
+func_professor() {
+    echo "CONFIGURING SYSTEM FOR A PROFESSOR"
+    sudo -i
+    echo "INFO: creating user professor"
+    # adduser professor
+    echo "INFO: adding user professor to sudo group"
+    # usermod -aG sudo professor
+    exit
+    echo "Installing Open Board"
+    # sudo snap install -y -qq openboard
+    echo "INFO: installing whatsapp"
+    # sudo snap install -y -qq whatsapp-for-linux
+}
+
 if [[ $Profession = "student" ]]
 then
-    echo Installing Git
-    sudo apt-get install -y git
-    echo Installing Google chrome
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install -y -qq ./google-chrome-stable_current_amd64.deb
-    echo Installing Java
-    sudo apt-get install -y -qq openjdk-11-jdk
-    echo Installing VLC Player
-    sudo snap install -y -qq vlc
-    echo Installing VS Code
-    sudo snap install -y -qq --classic code
-    echo Installing Whatsapp
-    sudo snap install -y -qq whatsapp-for-linux
-    echo Installing OBS Studio
-    sudo apt install -y -qq ffmpeg
-    sudo apt install -y -qq v4l2loopback-dkms
-    sudo add-apt-repository ppa:obsproject/obs-studio
-    sudo apt update
-    sudo apt install -y -qq obs-studio
-    echo Installing Discord
-    sudo snap install -y -qq discord
-
+    func_student()
+    func_commonInstall()
 elif [[ $Profession = "professor" ]]
 then
-    echo Installing LibreOffice
-    sudo apt install -y -qq libreoffice
-    echo Installing Google chrome
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo apt install -y -qq ./google-chrome-stable_current_amd64.deb
-    echo Installing Open Board
-    sudo apt install -y -qq snapd
-    sudo snap install -y -qq openboard
-    echo Installing Zoom
-    sudo apt install -y -qq ./zoom_amd64.deb
-    echo Installing Whatsapp
-    sudo snap install -y -qq whatsapp-for-linux
+    func_professor()
+    func_commonInstall()
 else
-    echo Invalid choice, terminating script
-    exit
-      
+    echo "WARN: invalid account type"
+    exit 
 fi
 
 
